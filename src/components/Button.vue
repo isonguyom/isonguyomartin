a7566ceaa3c959bd16b7f379c87dfb0f165bcc39
isonguyom <isonguyom@gmail.com>
2023-04-16 16:52:29 +0100

    feat: create Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,32 @@
+<template>
+    <a class="btn" :href="link">{{ text }}</a>
+</template>
+
+<script>
+export default {
+    props: ['text', 'link'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.btn {
+    display: block;
+    text-align: center;
+    text-transform: capitalize;
+    background-color: var(--primary-color);
+    color: var(--title-light);
+    max-width: 100%;
+    padding: 1em;
+    border-radius: 10px;
+
+
+    &:hover {
+        text-decoration: none;
+    }
+}
+</style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,23 +2,29 @@
     <section class="section-wrapper Hero">
         <div class="hero-text">
             <h1 class="hero-caption caption">Frontend Developer</h1>
-            <p class="para hero-para">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa vero iste ut sed asperiores nam saepe nulla amet. Necessitatibus repellendus magnam deserunt assumenda esse aspernatur et ducimus error consequatur dicta?</p>
-        <div class="socials">
-            <a v-for="(link, key) in socialLinks" :key="link.name" class="social-link" :href="link.link">{{ link.icon }}</a>
-            <a class="social-link" href="http://" target="_blank" rel="noopener noreferrer">d</a>
-        </div>
+            <p class="para hero-para">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa vero iste ut sed
+                asperiores nam saepe nulla amet. Necessitatibus repellendus magnam deserunt assumenda esse aspernatur et
+                ducimus error consequatur dicta?</p>
+            <div class="socials">
+                <a v-for="(link, key) in socialLinks" :key="link.name" class="social-link" :href="link.link">{{ link.icon
+                }}</a>
+                <a class="social-link" href="http://" target="_blank" rel="noopener noreferrer">d</a>
+            </div>
+            <Button link="#" text="Hire me" />
         </div>
     </section>
 </template>
 
 <script>
+import Button from './Button.vue';
 export default {
+    components: { Button },
     data() {
         return {
             socialLinks: [
-                {name: 'Github', icon: 'G', link: 'https://github.com/isonguyom'},
-                {name: 'Behance', icon: 'Be', link: 'https://www.behance.net/martinisonguyo'},
-                {name: 'Twitter', icon: 't', link: 'https://twitter.com/IsonguyoM'},
+                { name: 'Github', icon: 'G', link: 'https://github.com/isonguyom' },
+                { name: 'Behance', icon: 'Be', link: 'https://www.behance.net/martinisonguyo' },
+                { name: 'Twitter', icon: 't', link: 'https://twitter.com/IsonguyoM' },
             ]
         }
     }
@@ -33,24 +39,35 @@ export default {
     background-color: #1b1b1b;
     background-blend-mode: soft-light;
 
+    .hero-text {
+        position: absolute;
+        top: 50%;
+        left: 0;
+        transform: translate(0, -50%);
+        -ms-transform: translate(0, -50%);
+        padding: 2em 5%;
+
     .hero-caption {
         color: var(--primary-color);
+        margin-bottom: 10px;
     }
 
     .hero-para {
         color: var(--text-light);
+        line-height: 1.6em;
     }
 
     .socials {
-        margin: 50px 0;
+        margin: 40px 0 50px;
+
         .social-link {
-            display: inline-block;
             color: var(--title-light);
             border: 1px solid var(--title-light);
             border-radius: 50%;
             width: 40px;
             height: 40px;
             display: inline-flex;
+            display: -webkit-inline-flex;
             align-items: center;
             justify-content: center;
             margin-right: 1.5em;
@@ -60,6 +77,25 @@ export default {
                 color: #fff;
                 border-color: #fff;
             }
+        }
+    }
+}
+}
+
+@media screen and (min-width: 768px) {
+    .Hero {
+
+        .hero-text {
+            width: 70%;
+        }
+    }
+}
+
+@media screen and (min-width: 992px) {
+    .Hero {
+
+        .hero-text {
+            width: 50%;
         }
     }
 }
