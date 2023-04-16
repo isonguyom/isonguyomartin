87b48f23a716b2253c660e42ec79deaf1030aa3b
isonguyom <isonguyom@gmail.com>
2023-04-16 09:31:30 +0100

    Setup base views and create NotFound view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFound from '../views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,14 +10,16 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+     // will match everything and put it under `$route.params.pathMatch`
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
   ]
 })
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="section-wrapper">
+        <Transition name="fade" appear>
+            <h1>Page Not Found</h1>
+        </Transition>
+    </section>
+</template>
+
+<style scoped lang="scss">
+.section-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+        color: red;
+        transition: all 0.5s ease;
+    }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(10px);
+}
+
+</style>
